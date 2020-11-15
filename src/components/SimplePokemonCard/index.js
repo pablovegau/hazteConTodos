7085671ae160288e99373130620f3c270b78c4f6
Pablo Vega <pablo.vega@tiendeo.com>
2020-11-15 17:18:11 +0100

    👌 IMPROVE: Use property loading and delete IOA from SimplePokemonCard component

--- src/components/SimplePokemonCard/index.js
+++ src/components/SimplePokemonCard/index.js
@@ -1,24 +1,17 @@
 import React from "react"
 import { Link } from "react-router-dom"
-
-import { useNearScreen } from "../../hooks/useNearScreen"
-
 import { CardContainer, Container } from "./styles"
 
 export const SimplePokemonCard = ({ name = "unown" }) => {
-  const { isNearScreen, fromRef } = useNearScreen()
   return (
-    <Container ref={fromRef}>
-      {isNearScreen ? (
-        <Link to={`/pokemon/${name}`}>
-          <CardContainer>
-            <img
-              src={`https://img.pokemondb.net/sprites/black-white/anim/normal/${name}.gif`}
-            />
-            {name}
-          </CardContainer>
-        </Link>
-      ) : null}
+    <Container>
+      {/* <Link to={`/pokemon/${name}`}> */}
+      <Link to={`/${name}`}>
+        <CardContainer>
+          <img loading="lazy" src={`https://img.pokemondb.net/sprites/black-white/anim/normal/${name}.gif`} />
+          {name}
+        </CardContainer>
+      </Link>
     </Container>
   )
 }
